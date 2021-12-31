90c5e9b8f82365d21a7019fc0b111a3f08d26fe0
LironLevi94 <lironlevi94@gmail.com>
2021-12-31 13:18:56 +0200

    add User class

---
+++ BookShop/src/bookshop/User.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bookshop;
+
+import java.io.*;
+
+/**
+ *
+ * @author liron
+ */
+public class User implements Serializable
+{
+    private String UserName;
+    private String Password;
+
+    public User(String UserName, String Password) { //constractor
+        this.UserName = UserName;
+        this.Password = Password;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String UserName) {
+        this.UserName = UserName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    
+}
+
