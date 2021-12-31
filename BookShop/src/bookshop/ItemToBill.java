bcc2f573956f8971dca3b7eeec4fcc450f662c84
LironLevi94 <lironlevi94@gmail.com>
2021-12-31 14:29:39 +0200

    add ItemToBill class

---
+++ BookShop/src/bookshop/ItemToBill.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bookshop;
+import java.io.*;
+/**
+ *
+ * @author liron
+ */
+public class ItemToBill implements Serializable
+{
+    public Book ItemToBuy;
+    public int ItemAmount;
+    public int TotalPrice;
+    
+}
