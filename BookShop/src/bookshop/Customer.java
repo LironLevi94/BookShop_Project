5f24f6757d83d2ede978c9b335bb44274ba9e1cf
LironLevi94 <lironlevi94@gmail.com>
2021-12-31 14:32:59 +0200

    add Customer class

---
+++ BookShop/src/bookshop/Customer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bookshop;
+
+import java.io.*;
+
+/**
+ *
+ * @author liron
+ */
+public class Customer implements Serializable
+{
+    public String Name;
+}
